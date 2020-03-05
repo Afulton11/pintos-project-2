9b6ff0205a7dc2a65d6c2448bc7e232c085af039
Andrew Fulton <aifulton@cougar.cs.ksu.edu>
2020-03-05 13:54:50 -0600

    Add process_control_block struct

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -1,9 +1,16 @@
 #ifndef USERPROG_PROCESS_H
 #define USERPROG_PROCESS_H
 
+#include "user/syscall.h"
 #include "threads/thread.h"
 
-tid_t process_execute (const char *file_name);
+/* represents a process in the OS, AKA PCB or Task Control Block */
+struct process_control_block {
+  pid_t pid;  /* pid of the process */
+  const char *cmd_line; /* the line used to execute this process */
+};
+
+tid_t process_execute (const char *cmd_line);
 int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
