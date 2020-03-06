dd8a5eaad1bbf79efd92a928d251fb428c289af5
Andrew Fulton <aifulton@cougar.cs.ksu.edu>
2020-03-06 11:32:18 -0600

    Add brackets to syscall switch

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -5,6 +5,8 @@
 #include "threads/thread.h"
 #include "threads/vaddr.h"
 
+void sys_write()
+
 static void syscall_handler (struct intr_frame *);
 
 void
@@ -21,44 +23,72 @@ syscall_handler (struct intr_frame *f UNUSED)
   if (is_user_vaddr(f->esp))
   {
     // valid virtual address, read the sys call number.
-    call_number = get_user(f->esp);
+    call_number = *(int*)get_user(f->esp);
   }
 
   switch (call_number)
   {
     case SYS_HALT:
+    {
       break;
+    }
     case SYS_EXIT:
+    {
       break;
+    }
     case SYS_EXEC:
+    {
       break;
+    }
     case SYS_WAIT:
+    {
       break;
+    }
     case SYS_CREATE:
+    {
       break;
+    }
     case SYS_REMOVE:
+    {
       break;
+    }
     case SYS_OPEN:
+    {
       break;
+    }
     case SYS_FILESIZE:
+    {
       break;
+    }
     case SYS_READ:
+    {
       break;
+    }
     case SYS_WRITE:
+    {
       break;
+    }
     case SYS_SEEK:
+    {
       break;
+    }
     case SYS_TELL:
+    {
       break;
+    }
     case SYS_CLOSE:
+    {
       break;
+    }
     default;
+    {
       /* 
        * invalid pointer, safely terminate program
        * DONT KERNEL PANIC.
        * Free the processes' resources and terminate it.
        */
       break;
+    }
   }
 
   printf ("system call[%d]!\n", call_number);
