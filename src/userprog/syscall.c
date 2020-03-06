10308626aa1813d9f5116552fdc31ab81eaec7a3
Andrew Fulton <aifulton@cougar.cs.ksu.edu>
2020-03-06 11:20:28 -0600

    create system call handler skeleton

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -20,8 +20,45 @@ syscall_handler (struct intr_frame *f UNUSED)
 
   if (is_user_vaddr(f->esp))
   {
-    // valid virtual address, read the number.
+    // valid virtual address, read the sys call number.
     call_number = get_user(f->esp);
+  }
+
+  switch (call_number)
+  {
+    case SYS_HALT:
+      break;
+    case SYS_EXIT:
+      break;
+    case SYS_EXEC:
+      break;
+    case SYS_WAIT:
+      break;
+    case SYS_CREATE:
+      break;
+    case SYS_REMOVE:
+      break;
+    case SYS_OPEN:
+      break;
+    case SYS_FILESIZE:
+      break;
+    case SYS_READ:
+      break;
+    case SYS_WRITE:
+      break;
+    case SYS_SEEK:
+      break;
+    case SYS_TELL:
+      break;
+    case SYS_CLOSE:
+      break;
+    default;
+      /* 
+       * invalid pointer, safely terminate program
+       * DONT KERNEL PANIC.
+       * Free the processes' resources and terminate it.
+       */
+      break;
   }
 
   printf ("system call[%d]!\n", call_number);
